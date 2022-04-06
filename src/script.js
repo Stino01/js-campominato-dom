5c9598ecbbb9b7c1baa3df02a24395a808b9f0f1
Stino01 <agostino.carrieri01@gmail.com>
2022-04-06 09:55:12 +0200

    fix string bombe and complete ex

--- src/script.js
+++ src/script.js
@@ -1,13 +1,16 @@
 const button = document.getElementById('btn');
 button.addEventListener('click', getData);
+const resultDiv = document.getElementById('result');
 const numeroBombe = 16;
 const bombe = [];
+let tentativiMax;
+let tentativi = 0;
 
 function random(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
 function getData() {
     let inputVal = document.getElementById('difficulty').value;
@@ -26,17 +29,8 @@ function getData() {
     console.log("celle per lato: ", latoSquare);
     generateBomb(numerobox);
     stampaGriglia(numerobox, latoSquare);
+    
 }
-
-function generateBomb(numerobox) {
-    while (bombe.length < numeroBombe) {
-      let arrayBomba = random(1, numerobox);
-      if (!bombe.includes(arrayBomba)) {
-        bombe.push(arrayBomba);
-      }
-    }
-    console.log(bombe)
-  }
 
 function stampaGriglia(numerobox, latoSquare) {
     let contenitore = document.getElementById('main-cont');
@@ -61,23 +55,63 @@ function generaCella(numerobox, latoSquare) {
     return box;
 }
 
-function colorBox() {
+function generateBomb(numerobox) {
+  tentativiMax = numerobox - numeroBombe;
+  console.log('tentativiMax: ', tentativiMax)
+  bombe.length = 0;
+  while (bombe.length < numeroBombe) {
+    let arrayBomba = random(1, numerobox);
+    if (!bombe.includes(arrayBomba)) {
+      bombe.push(arrayBomba);
+    }
+  }
+  
+  console.log(bombe)
+  return tentativiMax;
+}
+
+function colorBox(tentativiMax) {
     let num = parseInt(this.innerText);
+    tentativi++
     if (bombe.includes(num)) {
+      showBox();
       this.style.backgroundColor = "red";
       this.innerHTML = `<i class="fa-solid fa-bomb"></i>`;
+      partitaconclusa();
     } else {
       this.style.backgroundColor = "#6495ed";
+      if (tentativi === tentativiMax) {
+        resultDiv.innerHTML += `
+            <h1>Hai vinto!</h1>
+            <h3>${tentativi}</h3>`;
+            showBox();
+      }
     }
     this.classList.remove("pointer");
     this.removeEventListener("click", colorBox);
 }
 
-/* Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. I numeri nella lista delle bombe non possono essere duplicati.
-In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
-La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
-Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una b.
+function partitaconclusa() {
+  allBox = document.querySelectorAll('.box')
+  for (i = 0; i < allBox.length; i++){
+    allBox[i].classList.remove('pointer')
+    allBox[i].removeEventListener('click', colorBox);
+}
+  resultDiv.innerHTML = `
+  <h1>Hai perso!</h1>
+  <h3>Tentativi: ${tentativi}</h3>
+  `
+}
 
-BONUS:
-1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
-2- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste */
+function showBox(){
+  allBox = document.querySelectorAll('.box')
+  for (i = 0; i < allBox.length; i++){
+      let num = parseInt(allBox[i].innerText)
+      if(bombe.includes(num)){
+          allBox[i].style.background = 'red';
+          allBox[i].innerHTML = `<i class="fa-solid fa-bomb"></i>`;
+      } else {
+          allBox[i].style.background = '#6495ED';
+      }
+  }
+}
